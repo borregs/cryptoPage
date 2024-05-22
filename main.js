da33776a460284ceaffcad84d14bcdaa10f13a31
borregs <borregs@MasterNode>
2024-05-22 09:19:56 -0700

    workin fine

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ let burger_icon = document.querySelector('.burger-icon');
 let ul = document.querySelector("nav ul");
 burger_icon.addEventListener("click",()=>{
     ul.classList.toggle("active");
-})
+});
+$("section").addClass("shady");
+$("img").addClass("shady");
+$("section").addClass("shadows");
+$("img").addClass("glass-effect");
+
